416054aa54cdc1d849bfb560d23e4b741294dfb6
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-05 19:36:02 +0400

    Create project using Spring Boot

---
+++ src/test/java/org/pronet/shoppie/ShoppieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pronet.shoppie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
