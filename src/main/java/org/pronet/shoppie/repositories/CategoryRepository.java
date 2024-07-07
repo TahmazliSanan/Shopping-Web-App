cfc55f80427d6248009a56765997b40c69e6f86d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-07 14:13:56 +0400

    Create repository: CategoryRepository

---
+++ src/main/java/org/pronet/shoppie/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package org.pronet.shoppie.repositories;
+
+import org.pronet.shoppie.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Boolean existsByName(String name);
+}
