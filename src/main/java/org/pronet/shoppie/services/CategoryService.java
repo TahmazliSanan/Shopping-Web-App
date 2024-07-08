948998c7190f09ebeb6ea3aac9f3d0a9f5ae90ce
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-08 16:06:32 +0400

    Modify methods

--- src/main/java/org/pronet/shoppie/services/CategoryService.java
+++ src/main/java/org/pronet/shoppie/services/CategoryService.java
@@ -1,14 +1,16 @@
 package org.pronet.shoppie.services;
 
 import org.pronet.shoppie.entities.Category;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface CategoryService {
-    Category add(Category category);
+    Category add(Category category, MultipartFile file) throws IOException;
     Category getById(Long id);
     List<Category> getList();
-    Category edit(Category category);
+    Category edit(Category category, MultipartFile file) throws IOException;
     Boolean remove(Long id);
     Boolean existsCategoryByName(String name);
 }
--- src/main/java/org/pronet/shoppie/services/impls/CategoryServiceImpl.java
+++ src/main/java/org/pronet/shoppie/services/impls/CategoryServiceImpl.java
@@ -3,10 +3,19 @@ package org.pronet.shoppie.services.impls;
 import org.pronet.shoppie.entities.Category;
 import org.pronet.shoppie.repositories.CategoryRepository;
 import org.pronet.shoppie.services.CategoryService;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -17,8 +26,24 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public Category add(Category category) {
-        return categoryRepository.save(category);
+    public Category add(Category category, MultipartFile file) throws IOException {
+        String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
+        category.setImageName(imageName);
+        Category savedCategory = categoryRepository.save(category);
+        if (!ObjectUtils.isEmpty(savedCategory)) {
+            if (!Objects.requireNonNull(file).isEmpty()) {
+                File savedFile = new ClassPathResource("static/").getFile();
+                Path path = Paths.get(
+                        savedFile.getAbsolutePath() +
+                                File.separator +
+                                "category-images" +
+                                File.separator +
+                                file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            }
+            return category;
+        }
+        return null;
     }
 
     @Override
@@ -34,8 +59,27 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public Category edit(Category category) {
-        return categoryRepository.save(category);
+    public Category edit(Category category, MultipartFile file) throws IOException {
+        Category dbCategory = getById(category.getId());
+        String imageName = file.isEmpty() ? dbCategory.getImageName() : file.getOriginalFilename();
+        dbCategory.setName(category.getName());
+        dbCategory.setImageName(imageName);
+        dbCategory.setIsActive(category.getIsActive());
+        Category updatedCategory = categoryRepository.save(dbCategory);
+        if (!ObjectUtils.isEmpty(updatedCategory)) {
+            if (!file.isEmpty()) {
+                File savedFile = new ClassPathResource("static/").getFile();
+                Path path = Paths.get(
+                        savedFile.getAbsolutePath() +
+                                File.separator +
+                                "category-images" +
+                                File.separator +
+                                file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            }
+            return category;
+        }
+        return null;
     }
 
     @Override
