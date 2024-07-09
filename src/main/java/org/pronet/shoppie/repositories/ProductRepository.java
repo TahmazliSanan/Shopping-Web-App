7818d5b7c774ebb47ad2150ad602961998fc4582
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-09 20:55:35 +0400

    Create repository: ProductRepository

---
+++ src/main/java/org/pronet/shoppie/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package org.pronet.shoppie.repositories;
+
+import org.pronet.shoppie.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIsActiveTrue();
+    List<Product> findByCategory(String category);
+    Boolean existsByName(String name);
+}
