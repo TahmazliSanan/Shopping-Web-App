29aa4eda95a64de629c1f43dfec2647e9572090c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-10 14:55:36 +0400

    Modify method

--- src/main/java/org/pronet/shoppie/controllers/HomeController.java
+++ src/main/java/org/pronet/shoppie/controllers/HomeController.java
@@ -1,12 +1,25 @@
 package org.pronet.shoppie.controllers;
 
+import org.pronet.shoppie.services.CategoryService;
+import org.pronet.shoppie.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+    public HomeController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("categoryList", categoryService.getActiveTopCategoryList());
+        model.addAttribute("productList", productService.getActiveTopProductList());
         return "home/home";
     }
 }
