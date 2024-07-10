6b56d0ebd1cd87bf1fceb30194e22f30086e3cc8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-10 14:57:30 +0400

    Add native query

--- src/main/java/org/pronet/shoppie/repositories/CategoryRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/CategoryRepository.java
@@ -2,12 +2,15 @@ package org.pronet.shoppie.repositories;
 
 import org.pronet.shoppie.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM categories WHERE is_active = 1 ORDER BY id LIMIT 4")
+    List<Category> takeActiveTopCategories();
     List<Category> findByIsActiveTrue();
     Boolean existsByName(String name);
 }
--- src/main/java/org/pronet/shoppie/repositories/ProductRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/ProductRepository.java
@@ -2,12 +2,15 @@ package org.pronet.shoppie.repositories;
 
 import org.pronet.shoppie.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM products WHERE is_active = 1 ORDER BY id DESC LIMIT 4")
+    List<Product> takeActiveTopProducts();
     List<Product> findByIsActiveTrue();
     List<Product> findByCategory(String category);
     Boolean existsByName(String name);
