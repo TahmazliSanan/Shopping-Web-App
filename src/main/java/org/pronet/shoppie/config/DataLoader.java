726f1595fd16158a3c76b63e79289b827f3522cc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-11 12:55:56 +0400

    Create component: DataLoader

---
+++ src/main/java/org/pronet/shoppie/config/DataLoader.java
@@ -0,0 +1,37 @@
+package org.pronet.shoppie.config;
+
+import org.pronet.shoppie.entities.UserEntity;
+import org.pronet.shoppie.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        UserEntity admin = userRepository.findByRole("Admin");
+        if (admin == null) {
+            admin = new UserEntity(
+                    1L,
+                    "Sanan Tahmazli",
+                    "sanan.tahmazli2329@gmail.com",
+                    "+123456789",
+                    "Baku",
+                    "AZE",
+                    "1148",
+                    "Baku, Azerbaijan",
+                    passwordEncoder.encode("AdminOfShoppier2002")
+            );
+            admin.setRole("Admin");
+            userRepository.save(admin);
+        }
+    }
+}
