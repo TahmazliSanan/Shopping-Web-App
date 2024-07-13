99d8ed8d609b5e07617620c04f8c0fd91598c8c7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-13 20:24:23 +0400

    Create service: UserDetailsServiceImpl

---
+++ src/main/java/org/pronet/shoppie/services/impls/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package org.pronet.shoppie.services.impls;
+
+import org.pronet.shoppie.config.CustomUser;
+import org.pronet.shoppie.entities.UserEntity;
+import org.pronet.shoppie.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User is not found!");
+        }
+        return new CustomUser(user);
+    }
+}
