ef9df85507de830145ff1b21c39e0614dfe17916
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-18 00:28:01 +0400

    Create util: AccountUtils

---
+++ src/main/java/org/pronet/shoppie/utils/AccountUtils.java
@@ -0,0 +1,37 @@
+package org.pronet.shoppie.utils;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+@Component
+public class AccountUtils {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public Boolean sendMail(String url, String recipientEmail)
+            throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom("tehmezlisenan11@gmail.com", "Shoppie");
+        helper.setTo(recipientEmail);
+        String body = "<p>Hello,</p>" + "<p>You have requested to reset your password.</p>"
+                + "<p>Click the link below to change your password:</p>" + "<p><a href=\"" + url
+                + "\">Change my password</a></p>";
+        helper.setSubject("Reset Password");
+        helper.setText(body, true);
+        mailSender.send(message);
+        return true;
+    }
+
+    public static String generateUrl(HttpServletRequest request) {
+        String siteUrl = request.getRequestURL().toString();
+        return siteUrl.replace(request.getServletPath(), "");
+    }
+}
