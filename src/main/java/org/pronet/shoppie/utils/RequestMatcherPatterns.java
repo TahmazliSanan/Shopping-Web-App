6fe380087d11d4653e24374c86d2ec33a5d70672
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-18 00:28:38 +0400

    Create class: RequestMatcherPatterns

---
+++ src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
@@ -0,0 +1,17 @@
+package org.pronet.shoppie.utils;
+
+public class RequestMatcherPatterns {
+    public static final String[] NON_AUTH_MATCHERS = {
+            "/css/**", "/carousel/**", "/category-images/**",
+            "/product-images/**", "/profile-images/**",
+            "/", "/category/list", "/product/list",
+            "/sign-up-view", "/sign-up", "/sign-in",
+            "/forgot-password", "/forgot-password-view", "/reset-password"
+    };
+    public static final String[] USER_AUTH_MATCHERS = {
+            "/product/details/{id}"
+    };
+    public static final String[] ADMIN_AUTH_MATCHERS = {
+            "/admin/**"
+    };
+}
