c33782a4ee82a3a1bfdf552380c199f2dc43a00b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-18 13:37:24 +0400

    Create CartRepository

---
+++ src/main/java/org/pronet/shoppie/repositories/CartRepository.java
@@ -0,0 +1,8 @@
+package org.pronet.shoppie.repositories;
+
+import org.pronet.shoppie.entities.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByUserIdAndProductId(Long userId, Long productId);
+}
