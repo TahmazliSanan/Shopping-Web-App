ae30609ead6bb0fa986c31167bfa2fbb18596209
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-20 13:17:01 +0400

    Add new methods: findByUserId() and countByUserId()

--- src/main/java/org/pronet/shoppie/repositories/CartRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/CartRepository.java
@@ -3,6 +3,10 @@ package org.pronet.shoppie.repositories;
 import org.pronet.shoppie.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Cart findByUserIdAndProductId(Long userId, Long productId);
+    List<Cart> findByUserId(Long userId);
+    Long countByUserId(Long id);
 }
