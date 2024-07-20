37c822e25162fcbc30ccf4a2d9c54944f35791a1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-20 20:35:35 +0400

    Create BaseController

---
+++ src/main/java/org/pronet/shoppie/controllers/BaseController.java
@@ -0,0 +1,30 @@
+package org.pronet.shoppie.controllers;
+
+import org.pronet.shoppie.entities.UserEntity;
+import org.pronet.shoppie.services.CartService;
+import org.pronet.shoppie.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+@Controller
+public class BaseController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CartService cartService;
+
+    @ModelAttribute
+    public void getUserDetails(Principal principal, Model model) {
+        if (principal != null) {
+            String email = principal.getName();
+            UserEntity user = userService.getUserByEmail(email);
+            model.addAttribute("user", user);
+            Long countCart = cartService.getCountCart(user.getId());
+            model.addAttribute("countCart", countCart);
+        }
+    }
+}
