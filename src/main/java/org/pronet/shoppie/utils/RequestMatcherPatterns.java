88c7fee55be092c5200539823be7e083e4b89d61
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-21 22:12:05 +0400

    Modify request matcher

--- src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
+++ src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
@@ -9,7 +9,7 @@ public class RequestMatcherPatterns {
             "/forgot-password", "/forgot-password-view", "/reset-password"
     };
     public static final String[] USER_AUTH_MATCHERS = {
-            "/product/details/{id}", "/cart/**", "/order-cart"
+            "/product/details/{id}", "/cart/**", "/order/**"
     };
     public static final String[] ADMIN_AUTH_MATCHERS = {
             "/admin/**"
