eabd5cd9c04a608fc413c612bef0a503f6377a02
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-22 11:32:14 +0400

    Complete method body

--- src/main/java/org/pronet/shoppie/controllers/OrderController.java
+++ src/main/java/org/pronet/shoppie/controllers/OrderController.java
@@ -1,17 +1,21 @@
 package org.pronet.shoppie.controllers;
 
+import org.pronet.shoppie.entities.Cart;
 import org.pronet.shoppie.entities.OrderRequest;
 import org.pronet.shoppie.entities.UserEntity;
+import org.pronet.shoppie.services.CartService;
 import org.pronet.shoppie.services.OrderService;
 import org.pronet.shoppie.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/order")
@@ -20,6 +24,8 @@ public class OrderController {
     private OrderService orderService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private CartService cartService;
 
     private UserEntity getLoggedInUserDetails(Principal principal) {
         if (principal != null) {
@@ -31,7 +37,16 @@ public class OrderController {
     }
 
     @GetMapping("/view")
-    public String orderPage() {
+    public String orderPage(Principal principal, Model model) {
+        UserEntity user = getLoggedInUserDetails(principal);
+        List<Cart> cartList = cartService.getListByUser(user.getId());
+        model.addAttribute("cartList", cartList);
+        if (!cartList.isEmpty()) {
+            Double orderPrice = cartList.get(cartList.size() - 1).getTotalOrderPrice();
+            Double totalOrderPrice = cartList.get(cartList.size() - 1).getTotalOrderPrice() + 250 + 100;
+            model.addAttribute("orderPrice", orderPrice);
+            model.addAttribute("totalOrderPrice", totalOrderPrice);
+        }
         return "/order/order";
     }
 
@@ -45,7 +60,7 @@ public class OrderController {
     }
 
     @GetMapping("/success")
-    public String loadSuccess() {
+    public String successPage() {
         return "/order/success";
     }
 }
