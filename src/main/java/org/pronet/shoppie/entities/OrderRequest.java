834d004b955779b171f1e029f4291f3558bfb713
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-24 13:36:09 +0400

    Rename field

--- src/main/java/org/pronet/shoppie/entities/OrderRequest.java
+++ src/main/java/org/pronet/shoppie/entities/OrderRequest.java
@@ -13,7 +13,7 @@ public class OrderRequest {
     private String firstName;
     private String lastName;
     private String email;
-    private String mobileNo;
+    private String mobileNumber;
     private String address;
     private String city;
     private String state;
