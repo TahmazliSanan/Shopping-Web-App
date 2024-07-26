9777d8cc76781574afc50caab6c0381c25f3ea2b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-26 19:42:16 +0400

    Add new method: sendMailForOrderProcess()

--- src/main/java/org/pronet/shoppie/utils/AccountUtils.java
+++ src/main/java/org/pronet/shoppie/utils/AccountUtils.java
@@ -3,6 +3,7 @@ package org.pronet.shoppie.utils;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
+import org.pronet.shoppie.entities.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,6 +15,7 @@ import java.io.UnsupportedEncodingException;
 public class AccountUtils {
     @Autowired
     private JavaMailSender mailSender;
+    String contentMessage = null;
 
     public Boolean sendMail(String url, String recipientEmail)
             throws MessagingException, UnsupportedEncodingException {
@@ -28,6 +30,32 @@ public class AccountUtils {
         helper.setText(body, true);
         mailSender.send(message);
         return true;
+    }
+
+    public void sendMailForOrderProcess(Order order, String status)
+            throws MessagingException, UnsupportedEncodingException {
+        contentMessage = "<p>Hello [[name]],</p>"
+                + "<p>Thank you order <b>[[orderStatus]]</b>.</p>"
+                + "<p><b>Product Details:</b></p>"
+                + "<p>Name : [[productName]]</p>"
+                + "<p>Category : [[category]]</p>"
+                + "<p>Quantity : [[quantity]]</p>"
+                + "<p>Price : [[price]] $</p>"
+                + "<p>Payment Type : [[paymentType]]</p>";
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom("tehmezlisenan11@gmail.com", "Shoppie");
+        helper.setTo(order.getOrderAddress().getEmail());
+        contentMessage = contentMessage.replace("[[name]]",order.getOrderAddress().getFirstName());
+        contentMessage = contentMessage.replace("[[orderStatus]]",status);
+        contentMessage = contentMessage.replace("[[productName]]", order.getProduct().getName());
+        contentMessage = contentMessage.replace("[[category]]", order.getProduct().getCategory());
+        contentMessage = contentMessage.replace("[[quantity]]", order.getQuantity().toString());
+        contentMessage = contentMessage.replace("[[price]]", order.getPrice().toString());
+        contentMessage = contentMessage.replace("[[paymentType]]", order.getPaymentType());
+        helper.setSubject("Order Status");
+        helper.setText(contentMessage, true);
+        mailSender.send(message);
     }
 
     public static String generateUrl(HttpServletRequest request) {
