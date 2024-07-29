ee42b65b090dcec2aa10b4e5f0a15b0a5dde471a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-07-29 13:01:00 +0400

    Add new method: findByOrderId()

--- src/main/java/org/pronet/shoppie/repositories/OrderRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/OrderRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByOrderId(String orderId);
     List<Order> findByUserId(Long id);
 }
