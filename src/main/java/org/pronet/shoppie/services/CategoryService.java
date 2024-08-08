16abb3ad46b6eaeed96b0aaae79da874c9336ac0
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-08 20:36:45 +0400

    Remove unused object

--- src/main/java/org/pronet/shoppie/services/CategoryService.java
+++ src/main/java/org/pronet/shoppie/services/CategoryService.java
@@ -2,7 +2,6 @@ package org.pronet.shoppie.services;
 
 import org.pronet.shoppie.entities.Category;
 import org.springframework.data.domain.Page;
-import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
