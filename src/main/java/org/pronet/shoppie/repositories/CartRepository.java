05aeef508044850f650b2de4a6a1bd92eed9c5ca
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-08 21:47:07 +0400

    Add annotation

--- src/main/java/org/pronet/shoppie/repositories/CartRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/CartRepository.java
@@ -2,9 +2,11 @@ package org.pronet.shoppie.repositories;
 
 import org.pronet.shoppie.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Cart findByUserIdAndProductId(Long userId, Long productId);
     List<Cart> findByUserId(Long userId);
