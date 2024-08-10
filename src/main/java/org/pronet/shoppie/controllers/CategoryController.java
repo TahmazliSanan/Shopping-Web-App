9c7332fe063b209a11d8a8d47064b803a4f77e62
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-11 01:07:23 +0400

    Add new method: viewProductListPage()

--- src/main/java/org/pronet/shoppie/controllers/CategoryController.java
+++ src/main/java/org/pronet/shoppie/controllers/CategoryController.java
@@ -1,12 +1,15 @@
 package org.pronet.shoppie.controllers;
 
 import org.pronet.shoppie.entities.Category;
+import org.pronet.shoppie.entities.Product;
 import org.pronet.shoppie.services.CategoryService;
+import org.pronet.shoppie.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +21,8 @@ import java.util.List;
 public class CategoryController extends BaseController {
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private ProductService productService;
 
     @Override
     public void getUserDetails(Principal principal, Model model) {
@@ -40,5 +45,25 @@ public class CategoryController extends BaseController {
         model.addAttribute("isFirstPage", page.isFirst());
         model.addAttribute("isLastPage", page.isLast());
         return "category/category-list";
+    }
+
+    @GetMapping("/list/{id}/view-products")
+    public String viewProductListPage(
+            @PathVariable("id") Long id,
+            @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = "5") Integer pageSize,
+            Model model) {
+        Category foundedCategory = categoryService.getById(id);
+        Page<Product> page = productService.getActiveProductList(pageNumber, pageSize, foundedCategory.getName());
+        List<Product> productList = page.getContent();
+        model.addAttribute("productList", productList);
+        model.addAttribute("productListSize", productList.size());
+        model.addAttribute("pageNumber", page.getNumber());
+        model.addAttribute("pageSize", pageSize);
+        model.addAttribute("totalElements", page.getTotalElements());
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("isFirstPage", page.isFirst());
+        model.addAttribute("isLastPage", page.isLast());
+        return "product/view-product-list-by-category";
     }
 }
