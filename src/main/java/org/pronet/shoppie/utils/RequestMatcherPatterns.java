16f5da315103dd4895fb1537499e0a7380f74f1c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-11 01:08:33 +0400

    Add new request matcher

--- src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
+++ src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
@@ -4,7 +4,8 @@ public class RequestMatcherPatterns {
     public static final String[] NON_AUTH_MATCHERS = {
             "/css/**", "/carousel/**", "/category-images/**",
             "/product-images/**", "/profile-images/**",
-            "/", "/category/list", "/product/list", "/product/search",
+            "/", "/category/list", "/category/list/{id}/view-products",
+            "/product/list", "/product/search",
             "/sign-up-view", "/sign-up", "/sign-in",
             "/forgot-password", "/forgot-password-view", "/reset-password",
             "/my-profile", "/update-profile", "/change-password"
