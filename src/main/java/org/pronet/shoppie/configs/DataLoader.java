80fb385a55370e5429fffe338c5daae8a944d245
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-10 13:21:40 +0400

    Update DataLoader

--- src/main/java/org/pronet/shoppie/configs/DataLoader.java
+++ src/main/java/org/pronet/shoppie/configs/DataLoader.java
@@ -7,19 +7,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class DataLoader implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) {
-        UserEntity admin = userRepository.findByRole("Admin");
-        if (admin == null) {
-            admin = new UserEntity(
+        List<UserEntity> adminList = userRepository.findAllByRole("Admin");
+        if (adminList.isEmpty()) {
+            UserEntity newAdmin = new UserEntity(
                     1L,
                     "Sanan Tahmazli",
                     "tehmezlisenan11@gmail.com",
@@ -38,7 +39,8 @@ public class DataLoader implements CommandLineRunner {
                     null,
                     null
             );
-            userRepository.save(admin);
+            adminList.add(newAdmin);
         }
+        userRepository.saveAll(adminList);
     }
 }
