56bcdf192498f5db299fd507fce05e88ba49d6b9
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-12 22:22:11 +0400

    Modify request matchers

--- src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
+++ src/main/java/org/pronet/shoppie/utils/RequestMatcherPatterns.java
@@ -5,8 +5,7 @@ public class RequestMatcherPatterns {
             "/css/**", "/carousel/**", "/category-images/**",
             "/product-images/**", "/profile-images/**",
             "/", "/category/list", "/category/list/{id}/view-products",
-            "/product/list", "/product/search",
-            "/sign-up-view", "/sign-up", "/sign-in"
+            "/product/list", "/sign-up-view", "/sign-up", "/sign-in"
     };
     public static final String[] USER_AUTH_MATCHERS = {
             "/product/details/{id}", "/cart/**", "/order/**"
@@ -15,8 +14,8 @@ public class RequestMatcherPatterns {
             "/admin/**"
     };
     public static final String[] USER_AND_ADMIN_AUTH_MATCHERS = {
-            "/delete-account", "/delete-profile-photo", "/forgot-password",
-            "/forgot-password-view", "/reset-password", "/my-profile",
-            "/update-profile", "/change-password", "/settings"
+            "/delete-account", "/delete-profile-photo", "/change-password",
+            "/forgot-password", "/forgot-password-view", "/reset-password",
+            "/my-profile", "/update-profile", "/change-password", "/settings"
     };
 }
