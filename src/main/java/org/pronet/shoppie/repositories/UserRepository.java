4f50bd3f7fe6658b8aa5f4794a6c4d0215328c36
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-13 20:06:25 +0400

    Add new methods: findByFullNameContainingOrEmailContainingAndRoleAdmin() and findByFullNameContainingOrEmailContainingAndRoleUser()

--- src/main/java/org/pronet/shoppie/repositories/UserRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package org.pronet.shoppie.repositories;
 
 import org.pronet.shoppie.entities.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByResetToken(String token);
     List<UserEntity> findAllByRole(String role);
+    Page<UserEntity> findAllByRole(Pageable pageable, String role);
+    @Query("SELECT u FROM UserEntity u WHERE (u.fullName LIKE %:character% OR u.email LIKE %:character%) AND u.role = 'Admin'")
+    Page<UserEntity> findByFullNameContainingOrEmailContainingAndRoleAdmin(Pageable pageable, String character);
+    @Query("SELECT u FROM UserEntity u WHERE (u.fullName LIKE %:character% OR u.email LIKE %:character%) AND u.role = 'User'")
+    Page<UserEntity> findByFullNameContainingOrEmailContainingAndRoleUser(Pageable pageable, String character);
 }
