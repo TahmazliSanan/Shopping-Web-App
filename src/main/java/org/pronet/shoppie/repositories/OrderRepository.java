37b8bd9e1f2447337ddf73829499900f53356fd1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-13 20:15:15 +0400

    Add new method: findByUserIdAndOrderId()

--- src/main/java/org/pronet/shoppie/repositories/OrderRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/OrderRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findByOrderId(String orderId);
     Page<Order> findByUserId(Long id, Pageable pageable);
+    Page<Order> findByUserIdAndOrderId(Long userId, Pageable pageable, String orderId);
 }
