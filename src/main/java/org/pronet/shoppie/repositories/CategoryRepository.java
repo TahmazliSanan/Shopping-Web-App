3a7677c21ed56d9cb1cc71ffd74effd816651571
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-08-13 20:10:52 +0400

    Add new methods: findByNameContainingIgnoreCase() and findByIsActiveTrueAndNameContainingIgnoreCase()

--- src/main/java/org/pronet/shoppie/repositories/CategoryRepository.java
+++ src/main/java/org/pronet/shoppie/repositories/CategoryRepository.java
@@ -1,6 +1,8 @@
 package org.pronet.shoppie.repositories;
 
 import org.pronet.shoppie.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,8 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Query(nativeQuery = true, value = "SELECT * FROM categories WHERE is_active = 1 ORDER BY id LIMIT 4")
     List<Category> takeActiveTopCategories();
     List<Category> findByIsActiveTrue();
+    Page<Category> findByNameContainingIgnoreCase(String character, Pageable pageable);
+    Page<Category> findByIsActiveTrue(Pageable pageable);
+    Page<Category> findByIsActiveTrueAndNameContainingIgnoreCase(String character, Pageable pageable);
     Boolean existsByName(String name);
 }
